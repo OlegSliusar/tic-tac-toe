32f0045e378b0bfdb7c44c7fdaf0fe3d097c5baf
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-08 14:27:25 +0200

    Add class and methods

---
+++ tic_tac_toe.rb
@@ -0,0 +1,105 @@
+class TicTacToe
+
+  def initialize
+    @players_move = rand(2)
+    @players_move = "X" if @players_move == 0
+    @players_move = "O" if @players_move == 1
+    @grid = []
+    puts "Player #{@players_move} starts the game..."
+    play
+  end
+
+  def show_current_grid
+    counter_three = 0
+    puts "\n"
+    puts "#" * 20
+    puts "\n"
+    puts "     1    2     3"
+    9.times do |i|
+      case i
+      when 0
+        print " a "
+      when 3
+        print " b "
+      when 6
+        print " c "
+      else
+        print ""
+      end
+      if @grid[i].nil?
+        print " " * 3
+      else
+        print " #{@grid[i]} "
+      end
+      print " | " unless counter_three == 2
+      counter_three += 1
+      if counter_three == 3
+        print "\n"
+        puts "   " + "-" * 15 unless i > 6
+        counter_three = 0
+      end
+    end
+    puts "\n"
+    puts "#" * 20
+  end
+
+  def move(input)
+    input = input.strip.downcase.scan(/\w/).join
+    case input
+    when "a1", "1a"
+      return puts "The place is taken. Try another one..." unless @grid[0].nil?
+      @grid[0] ||= @players_move
+    when "a2", "2a"
+      return puts "The place is taken. Try another one..." unless @grid[1].nil?
+      @grid[1] ||= @players_move
+    when "a3", "3a"
+      return puts "The place is taken. Try another one..." unless @grid[2].nil?
+      @grid[2] ||= @players_move
+    when "b1", "1b"
+      return puts "The place is taken. Try another one..." unless @grid[3].nil?
+      @grid[3] ||= @players_move
+    when "b2", "2b"
+      return puts "The place is taken. Try another one..." unless @grid[4].nil?
+      @grid[4] ||= @players_move
+    when "b3", "3b"
+      return puts "The place is taken. Try another one..." unless @grid[5].nil?
+      @grid[5] ||= @players_move
+    when "c1", "1c"
+      return puts "The place is taken. Try another one..." unless @grid[6].nil?
+      @grid[6] ||= @players_move
+    when "c2", "2c"
+      return puts "The place is taken. Try another one..." unless @grid[7].nil?
+      @grid[7] ||= @players_move
+    when "c3", "3c"
+      return puts "The place is taken. Try another one..." unless @grid[8].nil?
+      @grid[8] ||= @players_move
+    else
+      return puts "Wrong command."
+    end
+    swap_players
+  end
+
+  def swap_players
+    if @players_move == "O"
+      @players_move = "X"
+    elsif @players_move == "X"
+      @players_move = "O"
+    end
+  end
+
+  def play
+    until victory?
+      show_current_grid
+      print "Player #{@players_move}, enter your move: "
+      move(gets)
+    end
+  end
+
+  def victory?
+    false
+  end
+end
+
+
+# Test
+new_game = TicTacToe.new
