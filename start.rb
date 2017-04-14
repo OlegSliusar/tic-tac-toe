81b076f410a11dbf017445d7e7c77929af48408c
Oleh Sliusar <olehsliusar@gmail.com>
2017-04-14 21:27:16 +0300

    Move game interface to a different file

---
+++ start.rb
@@ -0,0 +1,22 @@
+require_relative 'lib/tic_tac_toe'
+
+game = TicTacToe.new
+
+loop do
+  until game.victory
+    game.show_current_board
+    print "Player #{@players_turn}, enter your move: "
+    game.move(gets)
+  end
+  game.show_current_board
+  puts "Player #{game.victory} wins!"
+  print "\n"
+  print "Play again? (y/n) "
+  answer = gets.strip
+  if answer == "y"
+    puts "#" * 90
+    game = TicTacToe.new
+  else
+    break
+  end
+end
