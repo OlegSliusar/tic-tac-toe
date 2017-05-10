825f115ddee738a48b0c6f640a0f23a650261b11
Oleh Sliusar <olehsliusar@gmail.com>
2017-05-10 08:37:23 +0300

    O wins even when X starts the game

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -56,9 +56,9 @@ describe TicTacToe do
       end
     end
 
-    context 'O wins' do
+    context 'O wins when doesn\'t start the game' do
       def make_moves_and_o_wins(moves)
-        @game.players_turn = 'O'
+        @game.players_turn = 'X'
         moves.each do |location|
           @game.move(location)
         end
@@ -66,42 +66,42 @@ describe TicTacToe do
       end
 
       it 'O wins on the top row' do
-        moves = ["a1", "b1", "a2", "b2", "a3"]
+        moves = ["c1", "a1", "b1", "a2", "b2", "a3"]
         make_moves_and_o_wins(moves)
       end
 
       it 'O wins on the middle row' do
-        moves = ["b1", "c1", "b2", "c2", "b3"]
+        moves = ["a1", "b1", "c1", "b2", "c2", "b3"]
         make_moves_and_o_wins(moves)
       end
 
       it 'O wins on the low row' do
-        moves = ["c1", "b1", "c2", "b2", "c3"]
+        moves = ["a1", "c1", "b1", "c2", "b2", "c3"]
         make_moves_and_o_wins(moves)
       end
 
       it 'O wins on the first column' do
-        moves = ["a1", "a2", "b1", "b2", "c1"]
+        moves = ["a3", "a1", "a2", "b1", "b2", "c1"]
         make_moves_and_o_wins(moves)
       end
 
       it 'O wins on the second column' do
-        moves = ["a2", "a3", "b2", "b3", "c2"]
+        moves = ["a1", "a2", "a3", "b2", "b3", "c2"]
         make_moves_and_o_wins(moves)
       end
 
       it 'O wins on the third column' do
-        moves = ["a3", "a2", "b3", "b2", "c3"]
+        moves = ["a1", "a3", "a2", "b3", "b2", "c3"]
         make_moves_and_o_wins(moves)
       end
 
       it 'O wins on first diagonal' do
-        moves = ["a1", "a2", "b2", "a3", "c3"]
+        moves = ["c1", "a1", "a2", "b2", "a3", "c3"]
         make_moves_and_o_wins(moves)
       end
 
       it 'O wins on second diagonal' do
-        moves = ["a3", "a2", "b2", "a1", "c1"]
+        moves = ["b1", "a3", "a2", "b2", "a1", "c1"]
         make_moves_and_o_wins(moves)
       end
     end
